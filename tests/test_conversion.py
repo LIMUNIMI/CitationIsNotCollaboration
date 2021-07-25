ed1fd76019814823c632a9bdafb9c66dfe6ba630
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-07-25 11:13:28 +0200

    test neighbors in asciigraph and bvgraph

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,9 +1,16 @@
 """Test conversion functions"""
-from featgraph import conversion, pathutils
+from featgraph import conversion, pathutils, metadata, jwebgraph
 from tests import testutils
 from unittest import mock
 import unittest
 import os
+
+
+def check_neighbors(base_path: str, aid: str, neighbors) -> bool:
+  """Check neighbors of node"""
+  a = sorted(n.aid for n in metadata.Artist(base_path, aid=aid).neighbors)
+  b = sorted(neighbors)
+  return a == b
 
 
 class TestConversion(
@@ -25,22 +32,54 @@ class TestConversion(
 
     self.setup_pickles_fn()
 
+    # convert
     conversion.main(
       self.adjacency_path, self.metadata_path, self.base_path,
       "-l", "WARNING",
       *(() if jvm_path is None else ("--jvm-path", jvm_path)),
       )
+
+    # check neighbors from asciigraph
+    for k, v in self.adjacency_dict.items():
+      with self.subTest(check="neighbors", file="asciigraph", node=k):
+        self.assertTrue(check_neighbors(self.base_path, k, v))
+
+    # delete asciigraph
+    s = ("graph-txt",)
+    with self.subTest(check_exists=s):
+      self.assertTrue(os.path.isfile(self.path(*s)))
+    os.remove(self.path(*s))
+
+    # check neighbors from bvgraph
+    # <!> it fails for neighbors of node e <!>
+    failure_nodes = ["e"]
+    for k, v in self.adjacency_dict.items():
+      expected_failure = k in failure_nodes
+      with self.subTest(
+        check="neighbors", file="bvgraph", node=k,
+        expected_failure=expected_failure,
+      ):
+        b = jwebgraph.jvm_process_run(
+          check_neighbors, args=(self.base_path, k, v),
+          return_type="B", jvm_kwargs=dict(jvm_path=jvm_path),
+        )
+        if expected_failure:
+          b = not b
+        self.assertTrue(b)
+
+    # check and delete files
     for s in (
       ("followers", "txt"), ("graph",),
       ("ids", "txt"), ("offsets",),
       ("properties",), ("genre", "txt"),
-      ("graph-txt",),  ("name", "txt"),
+      ("name", "txt"),  # ("graph-txt",),
       ("popularity", "txt"), ("type", "txt"),
     ):
       with self.subTest(check_exists=s):
         self.assertTrue(os.path.isfile(self.path(*s)))
       os.remove(self.path(*s))
-    # clean up
+
+    # clean up directories and pickles
     os.rmdir(os.path.dirname(self.path()))
     os.remove(self.adjacency_path)
     os.remove(self.metadata_path)
