350f8d02b225a0d336185132d15b44f32928ebf6
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-10-05 17:54:30 +0200

    :alembic: Make bayesian comparisons between genre-wise centralities

--- featgraph/comparison.py
+++ featgraph/comparison.py
@@ -1,11 +1,15 @@
 """Compare centrality values across genres"""
-from featgraph import scriptutils, jwebgraph, logger
+from featgraph import scriptutils, jwebgraph, bayesian_comparison, pathutils, logger
+import logging
 from chromatictools import cli
+import itertools
 import importlib
 import sys
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
+import arviz
 
 metrics_dict = {
     "closeness":
@@ -61,12 +65,36 @@ def main(*argv):
   parser.add_argument("--keep-other",
                       action="store_true",
                       help="Keep the genre 'other'")
+  parser.add_argument("--sample-chains",
+                      type=int,
+                      default=4,
+                      help="Number of Markov chains for NUTS")
+  parser.add_argument("--sample-cores",
+                      type=int,
+                      default=None,
+                      help="Number of cores for NUTS. "
+                      "Defaults to the number of Markov chains")
+  parser.add_argument("--sample-draws",
+                      type=int,
+                      default=4000,
+                      help="Number of draws from NUTS")
+  parser.add_argument("--netcdf-path",
+                      metavar="PATH",
+                      default=None,
+                      help="The directory path for saving the "
+                      "inference data to file")
+  parser.add_argument("--netcdf-compress",
+                      action="store_true",
+                      help="Compress netcdf file")
   args = parser.custom_parse(argv)
   if args.metric not in metrics_dict:
     logger.error(
         "Undefined metric '%s'. Please, run with "
         "--help for a list of supported metrics", args.metric)
     return 1
+  if args.sample_cores is None:
+    args.sample_cores = args.sample_chains
+  tqdm_fn = (lambda x, *args, **kwargs: x) if args.tqdm is None else args.tqdm
 
   # Load BVGraph
   jwebgraph.start_jvm(jvm_path=args.jvm_path)
@@ -86,9 +114,9 @@ def main(*argv):
   else:
     k = args.metric
   logger.info("Dataframe:\n%s", df)
+  violin_order = df.groupby(
+      by=["genre"])[k].median().sort_values().iloc[::-1].index
   if args.violin_path:
-    violin_order = df.groupby(
-        by=["genre"])[k].median().sort_values().iloc[::-1].index
     plt.xticks(rotation=33)
     sns.violinplot(data=df, x="genre", y=k, order=violin_order)
     logger.info("Saving violin plot to '%s'", args.violin_path)
@@ -98,3 +126,36 @@ def main(*argv):
     plt.gcf().set_size_inches(size_inches)
     plt.savefig(args.violin_path)
     plt.clf()
+
+  for m in ("filelock",):
+    importlib.import_module(m)
+    logging.getLogger(m).setLevel("WARN")
+  # Perform comparison
+  genres = sorted(df["genre"].unique(), key=violin_order.to_list().index)
+  # tcomp_data = {}
+  if args.netcdf_path is not None:
+    os.makedirs(args.netcdf_path, exist_ok=True)
+  for gx, gy in tqdm_fn(itertools.combinations(genres, 2),
+                        total=len(genres) * (len(genres) - 1) // 2):
+    # if gx not in tcomp_data:
+    #   tcomp_data[gx] = {}
+    filepath_kh = None if args.netcdf_path is None else os.path.join(
+        args.netcdf_path, f"{gx}-{gy}.netcdf")
+    if filepath_kh is None or pathutils.notisfile(filepath_kh):
+      tcomp = bayesian_comparison.StudentTComparison().fit(
+          **{g: df[df["genre"] == g][k] for g in (gx, gy)})
+      # tcomp_data[gx][gy] =
+      tcomp_data = tcomp.sample(chains=args.sample_chains,
+                                        cores=args.sample_cores,
+                                        draws=args.sample_draws,
+                                        return_inferencedata=True,
+                                        progressbar=args.tqdm is not None)
+      if filepath_kh is not None:
+        logger.info("Saving netcdf file: %s (compress=%s)", filepath_kh,
+                    args.netcdf_compress)
+        # tcomp_data[gx][gy]
+        tcomp_data.to_netcdf(filepath_kh, compress=args.netcdf_compress)
+    else:
+      logger.info("Loading netcdf file: %s", filepath_kh)
+      tcomp_data = arviz.from_netcdf(filepath_kh)
+    # TODO: Do something with tcomp_data
