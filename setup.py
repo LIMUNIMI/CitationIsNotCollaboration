f8e1ecb5a9d10b8605b2c999b48ec718edea9456
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-10-08 18:17:25 +0200

    :arrow_down: Drop support for 3.9

--- setup.py
+++ setup.py
@@ -46,5 +46,5 @@ setuptools.setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  python_requires=">=3.7.0",
+  python_requires=">=3.7.0,<3.9",
 )
