c5c7ec2b04ceec7db6949648d1ffc4afc8054027
ChromaticIsobar <marcotiraboschi@hotmail.it>
2022-01-31 11:19:59 +0100

    :art: Plot transitions for connected component sizes too

--- paper_plots/plots.py
+++ paper_plots/plots.py
@@ -29,7 +29,7 @@ def main(*argv):
     for c in args.centrality_specs:
       getattr(graph, f"compute_{c}")()
     logger.info("Loading centralities dataframe")
-    df = graph.supergenre_dataframe(indegree=graph.indegrees(),
+    df = graph.supergenre_dataframe(indegrees=graph.indegrees(),
                                     pagerank=graph.pagerank(),
                                     closenessc=graph.closenessc(),
                                     harmonicc=graph.harmonicc())
@@ -37,24 +37,23 @@ def main(*argv):
     # Preprocess dataframe
     logger.info("Preprocessing centralities dataframe")
     df.drop((i for i, g in enumerate(df.genre) if g == "other"), inplace=True)
-    df[r"$\log_{10}$indegree"] = np.log10(df["indegree"])
-    df[r"$\log_{10}$pagerank"] = np.log10(df["pagerank"])
-    df[r"harmonic centrality"] = df["harmonicc"]
-    df[r"closeness centrality"] = df["closenessc"]
-    df.drop(columns=[
-        "indegree",
-        "pagerank",
-        "closenessc",
-        "harmonicc",
-    ],
-            inplace=True)
+    for k, (fn_k, fn, _, _, _) in args.centrality_specs.items():
+      if fn is None:
+        fn = lambda x: x
+      df[fn_k] = fn(df[k])
+
+    centralities = (
+        args.centrality_specs["indegrees"][0],
+        args.centrality_specs["pagerank"][0],
+        args.centrality_specs["harmonicc"][0],
+        args.centrality_specs["closenessc"][0],
+    )
 
     def median_order(df, k, column="genre"):
       return df.groupby(by=[column])[k].median().sort_values().iloc[::-1].index
 
     logger.info("Plotting centralities violin plots")
     # Structure figure
-    centralities = [c for c in df.columns if c not in ("aid", "genre")]
     _, axs = plt.subplots(
         1,
         len(centralities),
@@ -62,8 +61,9 @@ def main(*argv):
         gridspec_kw=dict(wspace=0.25),
     )
 
-    axs[centralities.index("harmonic centrality")].xaxis.set_major_formatter(
-        plots.ExponentFormatter(exponent=5))
+    axs[centralities.index(
+        args.centrality_specs["harmonicc"][0])].xaxis.set_major_formatter(
+            plots.ExponentFormatter(exponent=5))
 
     # Plot
     for i, (ax, k) in enumerate(zip(axs, centralities)):
@@ -105,48 +105,72 @@ def main(*argv):
 
   # --- Centrality transitions ------------------------------------------------
   transition_plot_fname = "centrality-transitions.pdf"
+  cc_sizes_fname = "cc-transitions.pdf"
   df, plot_comparison = parser.perform_threshold_comparison(
-      args, lambda *_: transition_plot_fname)
+      args, transition_plot_fname, cc_sizes_fname)
   if plot_comparison:
-    n = len(args.centrality_specs)
-    fig, ax = plt.subplots(2, n, sharex=True)
-    for i, (_, (_, _, norm, logy,
-                k)) in enumerate(args.centrality_specs.items()):
-      logger.info("Plotting %s", k)
-      plots.plot_centrality_transitions(
-          df,
-          k,
-          median=False,
-          norm=norm,
-          logy=logy,
-          fill_alpha=0.1,
-          cmap={
-              "celebrities": args.palette["hip-hop"],
-              "community leaders": args.palette["classical"],
-              "masses": args.palette["rock"],
-              "hip-hop": args.palette["hip-hop"],
-              "classical": args.palette["classical"],
-              "rock": args.palette["rock"],
-          },
-          legend_kw=dict(loc="upper left"),
-          ax=ax[:, i])
-      for a in ax[:, i]:
-        yl = a.set_ylabel("" if i else a.get_title())
-        yl.set_rotation(0)
-        yl.set_horizontalalignment("left")
-        a.yaxis.set_label_coords(-0.05, 1.05)
-        a.set_title("")
-        a.grid()
-        if i == n - 1:
-          pass
-        else:
-          a.get_legend().remove()
-      ax[0, i].set_xlabel("")
-      ax[0, i].set_title("\n".join(
-          (k, args.norm_str.get(norm, f"normalized by {norm}"))))
-    fig.set_size_inches(mpl.rcParams["figure.figsize"][0] * 2 *
-                        np.array([1, 2 / n]))
-    args.save_fig(transition_plot_fname)
+    for fname, centralities, scale, legend_kw in (
+        (
+            transition_plot_fname,
+            (
+                "indegrees",
+                "pagerank",
+                "harmonicc",
+                "closenessc",
+            ),
+            2,
+            dict(loc="upper left"),
+        ),
+        (
+            cc_sizes_fname,
+            (
+                "node_scc_sizes",
+                "node_wcc_sizes",
+            ),
+            1,
+            dict(loc="lower left"),
+        ),
+    ):
+      if args.must_write(fname):
+        n = len(centralities)
+        fig, ax = plt.subplots(2, n, sharex=True)
+        for i, k_s in enumerate(centralities):
+          _, _, norm, logy, k = args.centrality_specs[k_s]
+          logger.info("Plotting %s", k)
+          plots.plot_centrality_transitions(
+              df,
+              k,
+              median=False,
+              norm=norm,
+              logy=logy,
+              fill_alpha=0.1,
+              cmap={
+                  "celebrities": args.palette["hip-hop"],
+                  "community leaders": args.palette["classical"],
+                  "masses": args.palette["rock"],
+                  "hip-hop": args.palette["hip-hop"],
+                  "classical": args.palette["classical"],
+                  "rock": args.palette["rock"],
+              },
+              legend_kw=legend_kw,
+              ax=ax[:, i])
+          for a in ax[:, i]:
+            yl = a.set_ylabel("" if i else a.get_title())
+            yl.set_rotation(0)
+            yl.set_horizontalalignment("left")
+            a.yaxis.set_label_coords(-0.05, 1.05)
+            a.set_title("")
+            a.grid()
+            if i == n - 1:
+              pass
+            else:
+              a.get_legend().remove()
+          ax[0, i].set_xlabel("")
+          ax[0, i].set_title("\n".join(
+              (k, args.norm_str.get(norm, f"normalized by {norm}"))))
+        fig.set_size_inches(mpl.rcParams["figure.figsize"][0] * scale *
+                            np.array([1, 2 / n]))
+        args.save_fig(fname)
   # ---------------------------------------------------------------------------
 
   # --- Plot degree distribution ----------------------------------------------
